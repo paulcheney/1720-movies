007245869b832d1c5b2c5b2dd8485df78ce0711a
Paul Cheney <spartandesign.biz@gmail.com>
2025-12-04 20:32:21 -0700

    update

--- scripts/movies.js
+++ scripts/movies.js
@@ -2,40 +2,54 @@
 import { allmovies } from "../data/movies.mjs";
 console.log(allmovies);
 
+//Select items in HTML document
 const destination = document.querySelector("#showmovies");
+const dialogpopup = document.querySelector("#moreinfo")
+const popuptitle = document.querySelector(".title")
+const poprelease = document.querySelector(".release")
+const popupdirector = document.querySelector(".director")
+const popupstudio = document.querySelector(".studio")
+const popupsummary = document.querySelector(".summary")
+const closepopupbutton = document.querySelector('#moreinfo button');
 
+// HIDE THE DIALOG
+closepopupbutton.addEventListener('click', () => {
+  dialogpopup.close();
+});
+
+// DISPLAY ALL THE MOVIES
 allmovies.forEach((movie) => {
   //Park Photo and Name
   console.log(movie);
 
   //Build Each Movie Card
-  const movcard = document.createElement("div")
+  const moviecard = document.createElement("section")
+  //build the movie title
+  const movietitle = document.createElement("h2")
+  movietitle.innerText = movie.title
+  // build the movie photo
+  const movieimage = document.createElement("img")
+  movieimage.src = `images/${movie.image_url}`
+  movieimage.alt = movie.title
+  movieimage.width = "250"
+  movieimage.height = "350"
+  //build the movie button
+  const moviebutton = document.createElement("button")
+  moviebutton.innerText = 'Movie Details'
+  moviebutton.addEventListener('click', () => showStuff(movie));
 
-  const movimage = document.createElement("img")
-  movimage.src = `images/${movie.image_url}`
-  movimage.alt=movie.title
-  movimage.width="250"
-  movimage.height="350"
-  
-  const movbutton = document.createElement("button")
-  movbutton.innerText = 'Movie Details'
-  movbutton.addEventListener('click', () => showStuff(x));
-
-  movcard.appendChild(movbutton)
-  movcard.appendChild(movimage)
-  destination.appendChild(movcard);
+  moviecard.appendChild(movietitle)
+  moviecard.appendChild(movieimage)
+  moviecard.appendChild(moviebutton)
+  destination.appendChild(moviecard);
 });
 
 
 function showStuff(x) {
-  mytitle.innerHTML = x.title
-  myimg.src = `images/${x.image_url}`
-
-  size.innerHTML = `Serving Size<span>${x.serving_size_oz}oz</span>`
-
-  carb.innerHTML = `Carbohydrate<span>${x.carbohydrates_g}g</span>`
-
-  cal.innerHTML = `Calories<span>${x.carbohydrates_g}cal</span>`
-
-  mydialog.showModal()
+  popuptitle.innerHTML = x.title
+  poprelease.innerHTML = `<span>DATE:</span> ${x.release_date}`
+  popupdirector.innerHTML = `<span>DIRECTOR:</span> ${x.director}`
+  popupstudio.innerHTML = `<span>STUDIO:</span> ${x.studio}`
+  popupsummary.innerHTML = `<span>SUMMARY:</span> ${x.summary}`
+  dialogpopup.showModal()
 }
